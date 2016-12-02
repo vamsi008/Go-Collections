d951e67e3c4750c133163c89695704f72d3cfa3f
vamsichava <vamsi.chava@imaginea.com>
2016-12-02 12:31:59 +0530

    Modified the code.

--- List/LinkedList.go
+++ List/LinkedList.go
@@ -1,9 +1,29 @@
-package LinkedList
+package List
 
+
+
+const MAX_LENGHT = 10
 type Node struct{
 
 	Next *Node
 	Value interface{}
+}
+
+
+type ArrayList struct{
+	Value interface{}
+}
+
+func NewArrayList() ArrayList{
+	var a [MAX_LENGHT]string
+	list:=ArrayList{a}
+	return list
+}
+
+func (list *ArrayList ) put(){
+
+	
+
 }
 
 
